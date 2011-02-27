86196333f9ba7ac5c40ed27201f7449f55b32618
Carlo Teubner <carlo.teubner@gmail.com>
2011-02-27 11:53:35 +0000

    Upstream version 2010.1

---
+++ init.sh
@@ -0,0 +1,20 @@
+url=http://garglk.googlecode.com/files/gargoyle-2010.1-sources.zip
+zipname=gargoyle-2010.1-sources.zip
+dirname=src
+
+if [[ -f $zipname ]]; then
+    echo "$zipname already exists in current dir, using it"
+elif [[ $# -gt 0 ]]; then
+    cp $1 $zipname
+else
+    curl -o $zipname $url
+fi
+
+rm -rf $dirname
+unzip -q -d $dirname $zipname
+rm $zipname
+cd $dirname
+rm -rf terps/alan2 terps/alan3 terps/hugo licenses/HUGO* \
+       fonts/LuxiMono* garglk/lm?.hex garglk/LuxiMono.txt licenses/LUXI* \
+       support
+cd ..
